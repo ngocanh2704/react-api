912990d114575eb59ff19b8ff9f7e572e93e977d
unknown <ngocanh27041994@gmail.com>
2020-05-21 16:17:37 +0700

    Update router 3

--- src/components/Menu/Menu..js
+++ src/components/Menu/Menu..js
@@ -1,4 +1,52 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+
+const menus = [
+  {
+    name: "Trang Chủ",
+    to: "/",
+    exact: true,
+  },
+  {
+    name: "Quản Lý Sản Phẩm",
+    to: "/product-list",
+    exact: false,
+  },
+];
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+  return (
+    <Route
+      path={to}
+      exact={activeOnlyWhenExact}
+      children={({ match }) => {
+        var active = match ? "active" : "";
+        return (
+          <li className={active}>
+            <Link to={to}>{label}</Link>
+          </li>
+        );
+      }}
+    />
+  );
+};
+
+const showMenus = (menus) => {
+  var result = null;
+  if (menus.length > 0) {
+    result = menus.map((menu, index) => {
+      return (
+        <MenuLink
+          key={index}
+          to={menu.to}
+          label={menu.name}
+          activeOnlyWhenExact={menu.exact}
+        />
+      );
+    });
+  }
+  return result;
+};
 
 const Menu = () => {
   return (
@@ -6,14 +54,7 @@ const Menu = () => {
       <a href=" " className="navbar-brand">
         Call Api
       </a>
-      <ul className="nav navbar-nav">
-        <li className="active">
-          <a href=" ">Trang chủ</a>
-        </li>
-        <li>
-          <a href=" ">Quản lý sản phẩm</a>
-        </li>
-      </ul>
+      <ul className="nav navbar-nav">{showMenus(menus)}</ul>
     </div>
   );
 };
