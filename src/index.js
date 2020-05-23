f821795fe9ba8ef320f38687384712cde575b67a
unknown <ngocanh27041994@gmail.com>
2020-05-23 10:19:38 +0700

    import thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import allReducers from "./reducers";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+const middleware = [
+  thunk
+]
 
 const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  allReducers, composeWithDevTools(applyMiddleware(...middleware))
 );
 
 ReactDOM.render(
